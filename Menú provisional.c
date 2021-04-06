0e330eb6192b209280c7a12945823e50c9b5d3ca
Jorgebpinedo <j.bpinedo@alumnos.upm.es>
2021-04-06 15:07:37 +0200

    Actualización menú principal
    Completadas algunas funciones principales
    Falta:
    -Completar opciones
    -Mejorar legibilidad mediante funciones

--- Menú provisional.c
+++ Menú provisional.c
@@ -5,10 +5,14 @@ int main ()
 {
 
     int elegir[10];
-    char usuario[100];
+    char usuario[100]; //Si se puede almacenar en un vector de vectores o en una estructura mejor
+    char usuario1[100];
+    char usuario2[100];
+    char usuario3[100];
+    char nombre_partida[100];
     _Bool bucle[10]={1,1,1,1,1,1,1,1};
 
-    printf ("Juego Laberinto\n\n");
+    printf("Juego del laberinto\n\n");
 
     do
     {
@@ -26,28 +30,95 @@ int main ()
             case 1:
                 do
                 {
-                printf("1-<Nombre de usuario>\n");
+                printf("1-<Configuracion de la partida>\n");
                 printf("2-<Salir>\n");
                 scanf("%i",&elegir[1]);
                     if (elegir[1]==1)
                     {
-                        printf("Ingresa nombre de usuario: \n");
-                        scanf("%[^\n]",usuario);
+                        printf("1-<Individual>\n");
+                        printf("2-<Multijugador>\n");
+                        scanf("%i",&elegir[2]);
+                        if (elegir[2]==1)
+                        {
+                            printf("Nombre de usuario (la partida se guardara con el mismo nombre): \n"); //Crear función que realice esto
+                            scanf(" %[^\n]",usuario);
+                            strcpy(nombre_partida,usuario);
+                            printf("Modo de juego:\n");
+                            printf("1-<Modo 1>\n");
+                            printf("2-<Modo 2>\n");
+                            printf("3-<Modo 3>\n");
+                            scanf("%i",&elegir[3]);
+                            printf("Iniciando partida...\n");
+                            bucle[1]=0;
+                        }
+                        else if (elegir[2]==2)
+                        {
+                            printf("Nombre de la partdia: ");
+                            scanf(" %[^\n]",nombre_partida);
+                            printf("Numero de jugadores (2 o 3): \n");
+                            scanf("%i",&elegir[3]);
+                            switch (elegir[3])
+                            {
+                                case 2:
+                                    printf("Nombre usuario 1: ");
+                                    scanf(" %[^\n]",usuario1);
+                                    printf("\n Nmbre usuario 2: ");
+                                    scanf(" %[^\n]",usuario2);
+                                    printf("Modo de juego:\n");
+                                    printf("1-<Modo 1>\n");
+                                    printf("2-<Modo 2>\n");
+                                    printf("3-<Modo 3>\n");
+                                    scanf("%i",&elegir[3]);
+                                    printf("Iniciando partida...\n");
+                                    break;
+
+                                case 3:
+                                    printf("Nombre usuario 1: ");
+                                    scanf(" %[^\n]",usuario1);
+                                    printf("\nNmbre usuario 2: ");
+                                    scanf(" %[^\n]",usuario2);
+                                    printf("\nNombre usuario 3: ");
+                                    scanf(" %[^\n]",usuario3);
+                                    printf("Modo de juego:\n");
+                                    printf("1-<Modo 1>\n");
+                                    printf("2-<Modo 2>\n");
+                                    printf("3-<Modo 3>\n");
+                                    scanf("%i",&elegir[3]);
+                                    printf("Iniciando partida...\n");
+                                    break;
+
+                                default:
+                                    printf("Error!\n");
+                                    break;
+
+                            }
+                        }
+                        else
+                            printf("Error!\n");
                     }
+                    else if (elegir[1]=2)
+                        bucle[1]=bucle[0]=0;
                     else
-                        bucle[1]=0;
+                        printf("Error!\n");
                 }
                 while(bucle[1]);
                 break;
 
             case 2:
-                printf("1-<Nombre de usuario>\n");
+                printf("1-<Nombre de la partida>\n");
                 printf("2-<Salir>\n");
+                scanf("%i",&elegir[1]);
+                if (elegir[1]==1)
+                    printf("Cargando partida...");
+                else if (elegir[1]==2)
+                    bucle[0]=0;
+                else
+                    printf("Error!\n");
                 printf("Usa ficheros, por ahora no sabemos\n");
                 break;
 
             case 3:
-                printf("Usa ficheros, por ahora no sabemos\n");
+                printf("No hay puntuaciones!\n");
                 break;
 
             case 4:
